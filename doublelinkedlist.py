34c3473b078f8987bff79ef97e6a38b9ff973ed4
iaine <iain.emsley@kellogg.ox.ac.uk>
2023-01-19 15:03:43 +0000

    Added gitignore and Pythoin

---
+++ doublelinkedlist.py
@@ -0,0 +1,152 @@
+#implementation of the doubly linked list
+#@todo: remove print for error traces
+
+
+class Node():
+
+    def __init__(self, data):
+        self.item = data    
+        self.next = None    
+        self.previous = None
+
+class DoublyLinkedList:    
+    def __init__(self):
+        self.start_node = None 
+        self.header = 0  
+
+    #Iterator Function
+    def __iter__(self): 
+         node = self.start_node
+         while node:
+            yield node
+            node = node.next
+            if not node:
+                break
+             
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            self.header = 1
+        else:
+            raise DLLException("list is not empty")
+
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            self.insert_in_emptylist(data)
+            return
+        new_node = Node(data)
+        new_node.next = self.start_node
+        self.start_node.previous = new_node
+        self.start_node = new_node
+        self.header += 1
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            self.header = 1
+            return
+        n = self.start_node
+        while n.next is not None:
+            n = n.next
+        new_node = Node(data)
+        n.next = new_node
+        new_node.previous = n
+        self.header += 1
+
+    def delete_at_start(self):
+        if self.start_node is None:
+            self.header = 0
+            raise DLLException("The list has no element to delete")
+        if self.start_node.next is None:
+            self.start_node = None
+            self.header = 0
+            return
+        self.start_node = self.start_node.next
+        self.start_prev = None
+        self.header -= 0
+        
+
+    def delete_at_end(self):
+        if self.start_node is None:
+            self.header = 0
+            raise DLLException("The list has no element to delete")
+            #return 
+        if self.start_node.next is None:
+            self.start_node = None
+            self.header = 0
+            return
+        n = self.start_node
+        while n.next is not None:
+            n = n.next
+        n.previous.next = None
+        self.header -= 0
+
+    def traverse_list(self):
+        if self.start_node is None:
+            raise DLLException("List has no element")
+            #return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.next
+
+    def search_list(self, search_item):
+        position = 0
+        results = []
+        if self.start_node is None:
+            raise DLLException("List has no element")
+            #return
+        else:
+            n = self.start_node
+            while n is not None:
+                position += 1
+                if n.item == search_item:
+                    results.append(position)
+                n = n.next
+        return results
+
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            raise DLLException("The list has no element to delete")
+            #return 
+        p = self.start_node
+        q = p.next
+        p.next = None
+        p.previous = q
+        while q is not None:
+            q.previous = q.next
+            q.next = p
+            p = q
+            q = q.previous
+        self.start_node = p
+
+    def setdir(self, data, left, right):
+        if self.start_node is None:
+            new_node = Node(data)
+            new_node.next = left
+            new_node.previous = right
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.next = self.start_node
+        self.start_node.previous = new_node
+        self.start_node = new_node
+
+class DLLException(Exception):
+    pass
+
+
+#new_linked_list = DoublyLinkedList()
+#new_linked_list.insert_in_emptylist(50)
+#new_linked_list.insert_at_start(1)
+#new_linked_list.insert_at_end(10)
+#new_linked_list.insert_at_end(12)
+#new_linked_list.delete_at_end()
+#new_linked_list.delete_at_start()
+#new_linked_list.traverse_list()
+#new_linked_list.reverse_linked_list()
+#new_linked_list.traverse_list()
---
+++ test_doublelinkedlist.py
@@ -0,0 +1,77 @@
+import unittest
+from doublelinkedlist import Node, DoublyLinkedList, DLLException
+
+class TestCircularDLLTests (unittest.TestCase):
+
+    def test_create(self):
+        #Initialize the linked list with a new node
+        doublyLL = DoublyLinkedList()
+        self.assertEqual(doublyLL.header, 0)
+
+    def test_insert_one_element(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_in_emptylist(50)
+        self.assertEqual(doublyLL.header, 1)
+
+    def test_raise_error_insert_one_element_into_non_empty(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_in_emptylist(50)
+        self.assertRaises(DLLException, doublyLL.insert_in_emptylist, 51)
+
+    def test_insert_at_start_empty_list(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_start(4)
+        self.assertEqual(doublyLL.header, 1)
+        items = [node.item for node in doublyLL]
+        self.assertEqual(items[0], 4)
+
+    def test_insert_at_start_empty(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_in_emptylist(50)
+        doublyLL.insert_at_start(4)
+        self.assertEqual(doublyLL.header, 2)
+        items = [node.item for node in doublyLL]
+        self.assertEqual(items[0], 4)
+
+    def test_insert_at_start_(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_start(50)
+        doublyLL.insert_at_start(4)
+        self.assertEqual(doublyLL.header, 2)
+        items = [node.item for node in doublyLL]
+        self.assertEqual(items[0], 4)
+
+    def test_insert_at_end_with_list_one_item(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_end(4)
+        self.assertEqual(doublyLL.header, 1)
+        items = [node.item for node in doublyLL]
+        self.assertEqual(items[len(items)-1], 4)
+
+    def test_insert_at_end_with_more_than_one(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_end(10)
+        self.assertEqual(doublyLL.header, 1)
+        doublyLL.insert_at_end(4)
+        self.assertEqual(doublyLL.header, 2)
+        items = [node.item for node in doublyLL]
+        self.assertEqual(items[len(items)-1], 4)
+
+    def test_search_list_one_entry(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_start(7)
+        search = doublyLL.search_list(7)
+        self.assertEqual(len(search), 1)
+        self.assertEqual(search[0], 1)
+
+    def test_search_list_multiple_entries(self):
+        doublyLL = DoublyLinkedList()
+        doublyLL.insert_at_start(9)
+        doublyLL.insert_at_end(9)
+        search = doublyLL.search_list(9)
+        self.assertEqual(len(search), 2)
+        self.assertEqual(search[len(search)-1], 2)
+    
+
+if __name__ == '__main__':
+    unittest.main()
